d6b065afb036fe8c2a175d9657346865fe3cbf81
nbauma109 <nbauma109@github.com>
2022-10-23 13:11:34 +0200

    fine-tune manifest lookup for version

--- src/main/java/jd/core/VersionAware.java
+++ src/main/java/jd/core/VersionAware.java
@@ -3,23 +3,45 @@ package jd.core;
 import org.jd.core.v1.util.StringConstants;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.JarURLConnection;
 import java.net.URL;
+import java.util.Enumeration;
 import java.util.jar.Attributes;
 import java.util.jar.Manifest;
 
 public interface VersionAware {
 
     default String getVersion() throws IOException {
+        return getMainAttribute("JD-Core-Version");
+    }
+
+    default String getMainAttribute(String attributeName) throws IOException {
         String className = getClass().getSimpleName() + StringConstants.CLASS_FILE_SUFFIX;
         String classPath = getClass().getResource(className).toString();
         if (!classPath.startsWith("jar")) {
-            return "SNAPSHOT";
+            return findFirstMainAttribute(attributeName);
         }
         URL url = new URL(classPath);
         JarURLConnection jarConnection = (JarURLConnection) url.openConnection();
         Manifest manifest = jarConnection.getManifest();
         Attributes attributes = manifest.getMainAttributes();
-        return attributes.getValue("JD-Core-Version");
+        return attributes.getValue(attributeName);
+    }
+
+    default String findFirstMainAttribute(String attributeName) throws IOException {
+        Enumeration<URL> manifestURLs = getClass().getClassLoader().getResources("META-INF/MANIFEST.MF");
+        while (manifestURLs.hasMoreElements()) {
+            URL manifestUrl = manifestURLs.nextElement();
+            try (InputStream inputStream = manifestUrl.openStream()) {
+                Manifest manifest = new Manifest(inputStream);
+                Attributes attributes = manifest.getMainAttributes();
+                String attributeValue = attributes.getValue(attributeName);
+                if (attributeValue != null) {
+                    return attributeValue;
+                }
+            }
+        }
+        return "SNAPSHOT";
     }
 }
