a94f75eee0fa42972f0de9f5ec4cfda0fdb12dfd
nbauma109 <nbauma109@github.com>
2023-05-19 10:32:51 +0200

    updated VersionAwareTest

--- src/test/java/jd/core/VersionAwareTest.java
+++ src/test/java/jd/core/VersionAwareTest.java
@@ -12,5 +12,6 @@ public class VersionAwareTest implements VersionAware {
         assertEquals("Apache Commons BCEL", getMainAttribute(JavaClass.class, "Bundle-Name"));
         assertEquals("SNAPSHOT", getVersion());
         assertNotNull(getMainAttribute("JD-Util-Version"));
+        assertEquals("1.8", getMainAttribute("X-Compile-Source-JDK"));
     }
 }
