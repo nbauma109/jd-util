6a2fb8775224bca998bd75714a3ac8108650f30a
nbauma109 <nbauma109@github.com>
2023-05-21 21:22:49 +0200

    added ReturnStatementTest

---
+++ src/test/java/org/jd/core/v1/model/javasyntax/statement/ReturnStatementTest.java
@@ -0,0 +1,31 @@
+package org.jd.core.v1.model.javasyntax.statement;
+
+import org.jd.core.v1.service.converter.classfiletojavasyntax.visitor.TestVisitor;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class ReturnStatementTest {
+
+    private ReturnStatement returnStatement;
+    private TestVisitor visitor;
+
+    @Before
+    public void setUp() {
+        returnStatement = ReturnStatement.RETURN;
+        visitor = new TestVisitor();
+    }
+
+    @Test
+    public void testReturnStatement() {
+        // Act
+        returnStatement.accept(visitor);
+
+        // Assert
+        assertEquals(1, visitor.getReturnStatementCount());
+        assertTrue(returnStatement.isReturnStatement());
+        assertEquals("ReturnStatement{}", returnStatement.toString());
+    }
+}
