153284ca81586d3b417b3231a710a924440d37ff
nbauma109 <nbauma109@github.com>
2023-05-21 17:00:27 +0200

    added testSuperExpression

---
+++ src/test/java/SuperExpressionTest.java
@@ -0,0 +1,38 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.jd.core.v1.model.javasyntax.expression.SuperExpression;
+import org.jd.core.v1.model.javasyntax.type.ObjectType;
+import org.jd.core.v1.service.converter.classfiletojavasyntax.visitor.TestVisitor;
+
+public class SuperExpressionTest {
+
+    @Test
+    public void testSuperExpression() {
+        // Arrange
+        int lineNumber = 10;
+        ObjectType type = ObjectType.TYPE_STRING_BUILDER;
+
+        // Act
+        SuperExpression expression = new SuperExpression(lineNumber, type);
+
+        // Assert
+        Assert.assertEquals(lineNumber, expression.getLineNumber());
+        Assert.assertEquals(type, expression.getType());
+        Assert.assertTrue(expression.isSuperExpression());
+
+        // Test accept method
+        TestVisitor visitor = new TestVisitor();
+        expression.accept(visitor);
+        Assert.assertEquals(1, visitor.getSuperExpressionCount());
+
+        // Test toString method
+        String expectedToString = "SuperExpression{" + type + "}";
+        Assert.assertEquals(expectedToString, expression.toString());
+
+        // Test copyTo method
+        int newLineNumber = 20;
+        SuperExpression copiedExpression = (SuperExpression) expression.copyTo(newLineNumber);
+        Assert.assertEquals(newLineNumber, copiedExpression.getLineNumber());
+        Assert.assertEquals(type, copiedExpression.getType());
+    }
+}
