f1449069b47d95eb6cf09cf98ad0c2de462f529d
nbauma109 <nbauma109@github.com>
2023-05-21 19:14:52 +0200

    added MemberDeclarationsTest

---
+++ src/test/java/org/jd/core/v1/model/javasyntax/declaration/MemberDeclarationsTest.java
@@ -0,0 +1,32 @@
+package org.jd.core.v1.model.javasyntax.declaration;
+
+import org.jd.core.v1.service.converter.classfiletojavasyntax.visitor.TestDeclarationVisitor;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MemberDeclarationsTest {
+    @Test
+    public void testMemberDeclarations() {
+        // Arrange
+        MemberDeclaration declaration1 = new StaticInitializerDeclaration("", null);
+        MemberDeclaration declaration2 = new StaticInitializerDeclaration("", null);
+        MemberDeclarations declarations = new MemberDeclarations(declaration1, declaration2);
+
+        // Assert
+        assertEquals(2, declarations.size());
+        assertEquals(declaration1, declarations.get(0));
+        assertEquals(declaration2, declarations.get(1));
+        assertEquals(0, new MemberDeclarations(10).size());
+
+        // Test accept
+        TestDeclarationVisitor visitor = new TestDeclarationVisitor();
+        declarations.accept(visitor);
+        assertEquals(1, visitor.getMemberDeclarationsCount());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testMemberDeclarationsException() {
+        new MemberDeclarations(new StaticInitializerDeclaration("", null));
+    }
+}
