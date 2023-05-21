514c15076ee6eb8b899bf80320da744f012dfb84
nbauma109 <nbauma109@github.com>
2023-05-21 10:24:57 +0200

    added BreakStatementTest

---
+++ src/test/java/org/jd/core/v1/model/javasyntax/statement/BreakStatementTest.java
@@ -0,0 +1,29 @@
+package org.jd.core.v1.model.javasyntax.statement;
+
+import org.jd.core.v1.service.converter.classfiletojavasyntax.visitor.TestVisitor;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+public class BreakStatementTest {
+
+    @Test
+    public void testBreakStatement() {
+        // Test BreakStatement with label
+        BreakStatement breakStatementWithLabel = new BreakStatement("myLabel");
+        assertEquals("myLabel", breakStatementWithLabel.getLabel());
+        assertTrue(breakStatementWithLabel.isBreakStatement());
+
+        // Test BreakStatement without label (using static BREAK)
+        BreakStatement breakStatementWithoutLabel = BreakStatement.BREAK;
+        assertNull(breakStatementWithoutLabel.getLabel());
+        assertTrue(breakStatementWithoutLabel.isBreakStatement());
+
+        // Test the accept method with a simple visitor
+        TestVisitor visitor = new TestVisitor();
+        breakStatementWithoutLabel.accept(visitor);
+        assertEquals(1, visitor.getBreakStatementCount());
+    }
+}
