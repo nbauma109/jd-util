a23c5a491cb16666f9349c94f64c9884d2fd61d7
nbauma109 <nbauma109@github.com>
2023-05-21 21:39:27 +0200

    added AnnotationReferencesTest

---
+++ src/test/java/org/jd/core/v1/model/javasyntax/reference/AnnotationReferencesTest.java
@@ -0,0 +1,39 @@
+package org.jd.core.v1.model.javasyntax.reference;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AnnotationReferencesTest {
+
+    private AnnotationReferences<AnnotationReference> annotationReferences;
+    private AnnotationReferences<AnnotationReference> annotationReferencesWithCapacity;
+    private TestReferenceVisitor visitor;
+
+    @Before
+    public void setUp() {
+        annotationReferences = new AnnotationReferences<>();
+        annotationReferencesWithCapacity = new AnnotationReferences<>(10);
+        visitor = new TestReferenceVisitor();
+    }
+
+    @Test
+    public void testAnnotationReferences() {
+        // Act
+        annotationReferences.accept(visitor);
+
+        // Assert
+        assertEquals(1, visitor.getVisitAnnotationReferencesCount());
+    }
+
+    @Test
+    public void testAnnotationReferencesWithCapacity() {
+        // Act
+        annotationReferencesWithCapacity.accept(visitor);
+
+        // Assert
+        assertEquals(1, visitor.getVisitAnnotationReferencesCount());
+        assertEquals(0, annotationReferencesWithCapacity.size());
+    }
+}
