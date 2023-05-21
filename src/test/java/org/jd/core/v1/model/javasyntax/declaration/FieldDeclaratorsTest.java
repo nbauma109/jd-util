149553088e72a775bd91244baadf78f4b5e6c3bb
nbauma109 <nbauma109@github.com>
2023-05-21 19:02:59 +0200

    added FieldDeclaratorsTest

---
+++ src/test/java/org/jd/core/v1/model/javasyntax/declaration/FieldDeclaratorsTest.java
@@ -0,0 +1,40 @@
+package org.jd.core.v1.model.javasyntax.declaration;
+
+import org.jd.core.v1.model.javasyntax.type.PrimitiveType;
+import org.jd.core.v1.service.converter.classfiletojavasyntax.visitor.TestDeclarationVisitor;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FieldDeclaratorsTest {
+    @Test
+    public void testFieldDeclarators() {
+        // Arrange
+        FieldDeclarator declarator1 = new FieldDeclarator("field1", new ExpressionVariableInitializer(null));
+        FieldDeclarator declarator2 = new FieldDeclarator("field2", new ExpressionVariableInitializer(null));
+        FieldDeclarators declarators = new FieldDeclarators(declarator1, declarator2);
+
+        // Assert
+        assertEquals(2, declarators.size());
+        assertEquals(declarator1, declarators.get(0));
+        assertEquals(declarator2, declarators.get(1));
+
+        // Act
+        FieldDeclaration fieldDeclaration = new FieldDeclaration(1, PrimitiveType.TYPE_INT, declarators);
+        declarators.setFieldDeclaration(fieldDeclaration);
+
+        // Assert - setFieldDeclaration
+        assertEquals(fieldDeclaration, declarator1.getFieldDeclaration());
+        assertEquals(fieldDeclaration, declarator2.getFieldDeclaration());
+
+        // Act & Assert - accept
+        TestDeclarationVisitor visitor = new TestDeclarationVisitor();
+        declarators.accept(visitor);
+        assertEquals(1, visitor.getFieldDeclaratorsCount());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testFieldDeclaratorsException() {
+        new FieldDeclarators(new FieldDeclarator("name"));
+    }
+}
