0f72bd72ce9e8791697cdd4c81f4f3e3d06b2d2c
nbauma109 <nbauma109@github.com>
2023-05-26 14:48:11 +0200

    added HyperlinkReferenceDataTest

---
+++ src/test/java/jd/core/links/HyperlinkReferenceDataTest.java
@@ -0,0 +1,24 @@
+package jd.core.links;
+
+import org.jd.core.v1.util.StringConstants;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class HyperlinkReferenceDataTest {
+
+    @Test
+    public void test() throws Exception {
+        ReferenceData referenceData = new ReferenceData(StringConstants.JAVA_LANG_OBJECT, "hashCode", "()I", null);
+        HyperlinkReferenceData hyperlinkReferenceData = new HyperlinkReferenceData(1, 2, referenceData);
+        assertEquals(1, hyperlinkReferenceData.getStartPosition());
+        assertEquals(3, hyperlinkReferenceData.getEndPosition());
+        assertFalse(hyperlinkReferenceData.isEnabled());
+        hyperlinkReferenceData.setEnabled(true);
+        assertTrue(hyperlinkReferenceData.isEnabled());
+        assertEquals(referenceData, hyperlinkReferenceData.getReference());
+        assertEquals("ReferenceData [typeName=java/lang/Object, name=hashCode, descriptor=()I, owner=null, enabled=false]", hyperlinkReferenceData.toString());
+    }
+}
