7db22581dfeed18a8b72d0af4655e679131d93dc
nbauma109 <nbauma109@github.com>
2023-05-26 14:58:45 +0200

    added BaseTypeTest

---
+++ src/test/java/org/jd/core/v1/model/javasyntax/type/BaseTypeTest.java
@@ -0,0 +1,25 @@
+package org.jd.core.v1.model.javasyntax.type;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+public class BaseTypeTest implements BaseType {
+
+    @Test
+    public void test() throws Exception {
+        assertFalse(isGenericType());
+        assertFalse(isInnerObjectType());
+        assertFalse(isObjectType());
+        assertFalse(isPrimitiveType());
+        assertFalse(isTypes());
+        assertEquals(ObjectType.TYPE_UNDEFINED_OBJECT, getOuterType());
+        assertEquals("", getInternalName());
+    }
+
+    @Override
+    public void accept(TypeVisitor visitor) {
+    }
+
+}
