9c08b7e4207c00ee6f5c7238fa8fd70c1b3203f5
nbauma109 <nbauma109@github.com>
2025-11-10 15:24:45 +0100

    removed jitci/jitpack scripts

--- jitci-build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-export JAVA_HOME=~/.sdkman/candidates/java/current
-./mvnw -B install -DskipTests -Dsigning.disabled=true --no-transfer-progress
--- jitci-test.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-export JAVA_HOME=~/.sdkman/candidates/java/current
-./mvnw -B test --no-transfer-progress
