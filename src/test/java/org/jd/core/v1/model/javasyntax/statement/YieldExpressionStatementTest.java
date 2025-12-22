4b407fea948126d3d7c87a2648af2b4fda2cb50b
nbauma109 <nbauma109@users.noreply.github.com>
2025-12-22 11:51:43 +0100

    Apply Eclipse cleanup refactoring (#140)
    Co-authored-by: nbauma109 <9403560+nbauma109@users.noreply.github.com>

--- src/test/java/org/jd/core/v1/model/javasyntax/statement/YieldExpressionStatementTest.java
+++ src/test/java/org/jd/core/v1/model/javasyntax/statement/YieldExpressionStatementTest.java
@@ -16,36 +16,36 @@ public class YieldExpressionStatementTest {
         // Test constructor with Expression
         Expression expression = new IntegerConstantExpression(1, PrimitiveType.TYPE_INT, 10);
         YieldExpressionStatement yieldExpressionStatement = new YieldExpressionStatement(expression);
-        
+
         assertTrue(yieldExpressionStatement.getExpression() instanceof IntegerConstantExpression);
         assertEquals(1, yieldExpressionStatement.getLineNumber());
         assertTrue(yieldExpressionStatement.isYieldExpressionStatement());
         assertEquals("YieldExpressionStatement{yield IntegerConstantExpression{type=PrimitiveType{primitive=int}, value=10}}", yieldExpressionStatement.toString());
-        
+
         // Test constructor with lineNumber and Expression
         YieldExpressionStatement yieldExpressionStatementWithLineNumber = new YieldExpressionStatement(2, expression);
         assertTrue(yieldExpressionStatementWithLineNumber.getExpression() instanceof IntegerConstantExpression);
         assertEquals(2, yieldExpressionStatementWithLineNumber.getLineNumber());
-        
+
         // Test setLineNumber and setExpression methods
         yieldExpressionStatementWithLineNumber.setLineNumber(3);
         Expression newExpression = new IntegerConstantExpression(1, 20);
         yieldExpressionStatementWithLineNumber.setExpression(newExpression);
-        
+
         assertEquals(3, yieldExpressionStatementWithLineNumber.getLineNumber());
         assertEquals(newExpression, yieldExpressionStatementWithLineNumber.getExpression());
-        
+
         // Test the getGenericExpression method
         Expression genericExpression = yieldExpressionStatementWithLineNumber.getGenericExpression();
         assertEquals(newExpression, genericExpression);
         assertTrue(genericExpression instanceof IntegerConstantExpression);
-        
+
         // Test the accept method with a simple visitor
         TestVisitor visitor = new TestVisitor();
         yieldExpressionStatementWithLineNumber.accept(visitor);
         assertEquals(1, visitor.getYieldExpressionStatementCount());
     }
-    
+
     @Test(expected = NullPointerException.class)
     public void testSetExpressionWithNull() {
         // Test the setExpression method with null
